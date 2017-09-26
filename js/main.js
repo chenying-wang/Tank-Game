78432666cb8ac9147dd3e953751044d525bc7d7a
mazett <wangchenying96@gmail.com>
2017-09-26 21:10:33 +0800

    Add vector and timer

--- js/main.js
+++ js/main.js
@@ -1,28 +1,64 @@
 const log = console.log.bind(console)
 
 class TankGame {
-    constructor(id) {
-        this._init(id)
+    constructor(id, grid) {
+        this._initCanvas(id)
+        this._initGrid(grid)
     }
 
     static new(...args) {
         return new this(...args)
     }
 
-    _init(id) {
+    _initCanvas(id) {
         this.id = id
         const canvas = document.querySelector('#' + id)
         if (canvas.getContext) {
+            this.totalWidth = canvas.width
+            this.totalHeight = canvas.height
             const ctx = canvas.getContext('2d')
             this.ctx = ctx
-            log(ctx)
         }
     }
 
+    _initGrid(grid) {
+        this.unitWidth = this.totalWidth / grid.x
+        this.unitHeight = this.totalHeight / grid.y
+    }
+
+    draw(vector, color) {
+        this.ctx.fillStyle = color
+        const x = vector.x * this.unitWidth
+        const y = this.totalHeight - this.unitHeight - vector.y * this.unitHeight
+        if (x < 0 || x > this.totalWidth || y < 0 || y > this.totalHeight) {
+            return
+        }
+        this.ctx.fillRect(x, y, this.unitWidth, this.unitHeight)
+    }
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.totalWidth, this.totalHeight)
+    }
+}
+
+const debug = tankGame => {
+    window.debugTankGame = tankGame
+    document.querySelector('#debug').style.visibility = 'visible'
+}
+
+const debugDraw = () => {
+    const x = document.querySelector('#x').value
+    const y = document.querySelector('#y').value
+    window.debugTankGame.draw(Vector.new(x, y), "rgb(0, 0, 0)")
 }
 
 const main = () => {
-    mTankGame = TankGame.new('game')
+    let mTankGame = TankGame.new('game', Vector.new(80, 45))
+    let timer = Timer.new(() => {
+        mTankGame.clear()
+    }, 1000)
+    timer.start()
+    debug(mTankGame)
 }
 
 window.onload = () => {
---
+++ js/timer.js
@@ -0,0 +1,31 @@
+class Timer {
+    constructor(run, interval) {
+        this.pause = true
+        this.lock = true
+        this.run = run
+        this.interval = interval
+        this._run()
+    }
+
+    static new(...args) {
+        return new this(...args)
+    }
+
+    start() {
+        this.pause = false
+        this._run()
+    }
+
+    stop() {
+        this.pause = true
+    }
+
+    _run() {
+        if(this.pause == false) {
+            this.run()
+            window.setTimeout(() => {
+                this._run()
+            }, this.interval)
+        }
+    }
+}
